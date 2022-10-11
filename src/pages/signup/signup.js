84f3a1620abf0843b4122398c39f5308063736d0
lizandrafmiazaki <lizandramiazaki@gmail.com>
2022-10-11 17:27:24 -0300

    função de criar usuário e função de logar o usuário q foi criado com e-mail e senha funcionando com SUCESSO

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,10 +1,12 @@
-import app from "../../config-firebase.js";
+import app from '../../config-firebase.js';
 import {
   getAuth,
   createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+  signInWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
+
 export default () => {
-  const container = document.createElement("div");
+  const container = document.createElement('div');
 
   const template = `
         <section class="container-signup">
@@ -13,39 +15,37 @@ export default () => {
             </figure>
             <h1>Cadastro</h1>
             <form class="form-signup">
-                <div class="name-lastname">
-                    <label for="insert-name" class="registration-label" id="label-name">Nome completo:</label>
-                    <input class="input-name-lastname" id="insert-name" placeholder="Insira seu nome e sobrenome"/>
-                    
-                    <label for="insert-username" class="registration-label">Nome de usuário:</label>
-                    <input class="input-username" id="insert-username" placeholder="Insira seu @username"/>
+              <label for="insert-name" class="registration-label" id="label-name">Nome:</label>
+              <input class="input-name" id="insert-name" placeholder="Insira seu nome"/>
+              
+              <label for="insert-username" class="registration-label">Nome de usuário:</label>
+              <input class="input-username" id="insert-username" placeholder="Insira seu @username"/>
 
-                    <label for="insert-email" class="registration-label">E-mail:</label>
-                    <input type="email" class="input-email" id="insert-email" placeholder="Insira seu e-mail"/>
+              <label for="insert-email" class="registration-label">E-mail:</label>
+              <input type="email" class="input-email" id="insert-email" placeholder="Insira seu e-mail"/>
 
-                    <label for="insert-password" class="registration-label">Senha:</label>
-                    <input type="password" class="input-password" id="insert-password" placeholder="Crie sua senha" />
-                    
-                    <label for="insert-confirm" class="registration-label">Senha:</label>
-                    <input type="password" class="input-confirmpassword" id="insert-confirm" placeholder="Confirme sua senha" />
+              <label for="insert-password" class="registration-label">Senha:</label>
+              <input type="password" class="input-password" id="insert-password" placeholder="Crie sua senha" />
+              
+              <input type="submit" class="btn" id="btn-cadastrar" value="Cadastrar"/>
 
-                    
-                    <input type="submit" class="btn-signup">Cadastrar</input>
-                    <button class="btn-clear">Limpar todos os campos</button>
+              <button class="btn" id="btn-clean">Limpar campos</button>            
+              
             </form>
-                <div class="btn-signup">
-
-                    <a href= "/#"><button class="btn" id="btn-initial">Retornar ao Login</button></a>
-                </div>
+            
+              <button class="btn" id="btn-initial">Retornar ao Login</button>
+            
         </section>`;
   container.innerHTML = template;
 
   // Eventos para capturar inputs:
-  const email = container.querySelector("#insert-email");
-  const password = container.querySelector("#insert-password");
-  const form = container.querySelector(".form-signup");
+  const inputName = container.querySelector('#insert-name');
+  const inputUsername = container.querySelector('#insert-username');
+  const email = container.querySelector('#insert-email');
+  const password = container.querySelector('#insert-password');
+  const form = container.querySelector('.form-signup');
 
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const auth = getAuth(app);
@@ -53,6 +53,7 @@ export default () => {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
+        window.location.hash = '#homepage';
         console.log(user);
         // ...
       })
@@ -61,9 +62,23 @@ export default () => {
         const errorMessage = error.message;
         // ..
       });
-    console.log("submit");
+    // signInWithEmailAndPassword(auth, email.value, password.value)
+    //   .then((userCredential) => {
+    //     // Signed in
+    //     const user = userCredential.user;
+    //     // ...
+    //   })
+    //   .catch((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //   });
+
+    //testando:
+    console.log('submit');
     console.log(email.value);
     console.log(password.value);
+    console.log(inputName.value);
+    console.log(inputUsername.value);
   });
 
   return container;
