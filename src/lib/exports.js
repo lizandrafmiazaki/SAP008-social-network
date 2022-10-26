aa1e6b8da84e05afe8a382fd94e575890e197650
lizandrafmiazaki <lizandramiazaki@gmail.com>
2022-10-26 12:11:22 -0300

    continuação da organização funções firebase

--- src/lib/exports.js
+++ src/lib/exports.js
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-unresolved */
+export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
+
 export {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
+  signOut,
 } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
 
-// export { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
+// export { getFirestore, collection, addDoc, doc, getDoc, getDocs, updateDoc, deletDoc, } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
